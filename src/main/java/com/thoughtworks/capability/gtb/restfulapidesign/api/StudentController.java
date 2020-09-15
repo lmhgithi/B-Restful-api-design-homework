a76e211db3b4bfd1d9f42e433c3745beca4f7a61
lmhgithi <lmhfox@foxmail.com>
2020-09-15 09:23:31 +0800

    [lmh] fix get students gender api

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentController.java
@@ -22,13 +22,12 @@ public class StudentController {
     }
 
     @GetMapping("/students")
-    public ResponseEntity<List<Student>> getStudents() {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudents());
-    }
-
-    @GetMapping("/students/genders/{gender}")
-    public ResponseEntity<List<Student>> getStudentsByGender(@PathVariable Gender gender) {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudentsByGender(gender));
+    public ResponseEntity<List<Student>> getStudents(@RequestParam(required = false) Gender gender) {
+        if(gender == null){
+            return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudents());
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudentsByGender(gender));
+        }
     }
 
     @GetMapping("/students/{name}")
